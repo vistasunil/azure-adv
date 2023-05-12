0c585f60a7aafa27edf590e40e780d717b32be43
vistasunil <37858762+vistasunil@users.noreply.github.com>
2023-05-12 15:17:22 +0200

    Update pick.controller.js

--- GameBot/api/controllers/pick.controller.js
+++ GameBot/api/controllers/pick.controller.js
@@ -15,10 +15,10 @@ const pick = async (req, res) => {
     const result = pickFromStrategy(strategyOption);
 
     // TODO: implement custom arcade intelligence here, see also ./GameBot/README.md for sample requests    
-    // if (player1Name == "Dud" && turn == 0) {
-    //    strategyOption = "CUSTOM";
-    //    result.text = "rock";
-    // }
+    if (player1Name == "Kye" && turn == 0) {
+        strategyOption = "CUSTOM";
+        result.text = "paper";
+    }
 
     console.log('Against ' + player1Name + ', strategy ' + strategyOption + '  played ' + result.text);
 
